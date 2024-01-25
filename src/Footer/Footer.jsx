c0a7e49079e525a4bd7c202955bf4a8d4222ded4
Irina Vedennikova <ivedennikova@gmail.com>
2024-01-25 18:03:20 +0100

    Replaced string with constant

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,13 +2,14 @@ import TargetLow from "./TargetLow";
 import TargetHigh from "./TargetHigh";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import {DEFAULT_ACTIVE_BUTTON} from "../Head";
 
 function Footer({ activePrice }) {
     return(
         <Row className="fixed-bottom bg-light justify-content-center">
             <Col className="text-center">
-                {activePrice === 'low' && <TargetLow />}
-                {activePrice !== 'low' && <TargetHigh />}
+                {activePrice === DEFAULT_ACTIVE_BUTTON && <TargetLow />}
+                {activePrice !== DEFAULT_ACTIVE_BUTTON && <TargetHigh />}
             </Col>
         </Row>
     );
